6ad225f513679e5784f634d10276328fb996c1a9
PaulAndre.Francisco <paullandre>
2022-06-05 21:46:28 +0800

    'js_css'

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,5 +1,18 @@
 @extends('layout')
- 
+<!DOCTYPE html>
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+-->
+{# HTML::style('css/bootstrap-theme.min.css') #}
+{# HTML::style('css/bootstrap.min.css') #}
+{# HTML::script('js/bootstrap.min.js') #}
+{# HTML::script('js/jquery.min.js') #}
+
+<link rel="stylesheet" href="{{asset('css/bootstrap-theme.min.css')}}">
+<script src="{{asset('js/jquery.min.js')}}"></script>
+
 @section('content')
 <table style="border: 1px solid grey">
     <tr>
@@ -17,4 +30,11 @@
 <style>
     td {border: 1px solid grey}
 </style>
+
+<script>
+$(document).ready(function(){
+    alert("TEST");
+});
+</script>
+</html>
 @stop
